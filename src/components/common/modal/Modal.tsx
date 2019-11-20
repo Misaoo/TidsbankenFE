17497f46e0461b7142f3aa2a14f3e310982acd7f
Victor Heijler <victor.heijler@se.experis.com>
2019-11-20 14:50:15 +0100

    Add close icon to modal

--- src/components/common/modal/Modal.tsx
+++ src/components/common/modal/Modal.tsx
@@ -1,5 +1,6 @@
 import React, { useEffect } from 'react';
 import styles from '../../../css/Modal.module.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 const Modal = (props: any) => {
 
@@ -20,7 +21,7 @@ const Modal = (props: any) => {
         return (
             <div className={styles.module} onClick={event => props.setDisplay(false)}>
                 <div className={styles.content} onClick={event => event.stopPropagation()}>
-                    <span className={styles.closeButton} onClick={event => props.setDisplay(false)}>x</span>
+                    <span className={styles.closeButton} onClick={event => props.setDisplay(false)}><FontAwesomeIcon icon="times" /></span>
                     { props.children }
                 </div>
             </div>
