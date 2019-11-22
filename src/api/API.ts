4757a196618e03c7ffa218109032f57b082b71aa
Elliot643 <elliot643@hotmail.com>
2019-11-22 10:30:55 +0100

    process.env error persists

--- src/api/API.ts
+++ src/api/API.ts
@@ -1,37 +1,42 @@
-import axios from 'axios';
+import axios from "axios";
+import * as process from "process";
 
-const login = (email: string, password: string) => axios(`${process.env.REACT_APP_API_URL}/login`, {
-    method: "POST", 
-    withCredentials: true, 
+const login = (email: string, password: string) =>
+  axios(`${process.env.REACT_APP_API_URL}/login`, {
+    method: "POST",
+    withCredentials: true,
     data: { email, password }
-});
+  });
 
-const login2fa = (token: string) => axios(`${process.env.REACT_APP_API_URL}/login2fa`, {
-    method: "POST", 
-    withCredentials: true, 
-    data: { "password2fa": token }
-});
+const login2fa = (token: string) =>
+  axios(`${process.env.REACT_APP_API_URL}/login2fa`, {
+    method: "POST",
+    withCredentials: true,
+    data: { password2fa: token }
+  });
 
-const logout = () =>  axios(`${process.env.REACT_APP_API_URL}/logout`, {
-    method: "POST", 
-    withCredentials: true
-});
-
-const authorize = () => axios(`${process.env.REACT_APP_API_URL}/authorize`, {
+const logout = () =>
+  axios(`${process.env.REACT_APP_API_URL}/logout`, {
     method: "POST",
     withCredentials: true
-});
+  });
 
-const user = (id: number) => axios(`${process.env.REACT_APP_API_URL}/user/${id}`, {
+const authorize = () =>
+  axios(`${process.env.REACT_APP_API_URL}/authorize`, {
+    method: "POST",
+    withCredentials: true
+  });
+
+const user = (id: number) =>
+  axios(`${process.env.REACT_APP_API_URL}/user/${id}`, {
     method: "GET",
     withCredentials: true
-});
+  });
 
-
-export default { 
-    login, 
-    login2fa, 
-    logout, 
-    authorize,
-    user 
+export default {
+  login,
+  login2fa,
+  logout,
+  authorize,
+  user
 };
