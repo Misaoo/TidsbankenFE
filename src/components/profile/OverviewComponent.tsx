fea57887c85f8da65ca09ba3cd69c7329433cbac
Rolandas <rolandas.janulis@se.experis.com>
2019-12-10 14:42:39 +0100

    OverviewComponent previous vacaiton days will be hiden if there is none

--- src/components/profile/OverviewComponent.tsx
+++ src/components/profile/OverviewComponent.tsx
@@ -1,10 +1,13 @@
-import React, { useContext, useState, useEffect } from "react";
+import React, { useContext, useState, useEffect, useRef } from "react";
 import AuthContext from "../auth/AuthContext";
 import API from "../../api/API";
 import vacationStyles from "../../css/profile/VacationComponent.module.css";
+
 import { Link } from "react-router-dom";
 import Collapse from '@material-ui/core/Collapse';
 import Button from '@material-ui/core/Button';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
 
 /*************/
 /* Displayes the: 
@@ -18,6 +21,7 @@ const OverviewComponent = (props: any) => {
   let [pendingVacationdays, setPendingVacationsdays] = useState(); 
   let [previousVacationdays, setPrevious] = useState<any[]>([]);          // handles the pending vacation days
   let [totalDeniedVacationRequests, setTotalDeniedVacationRequests] = useState();     // handles total denied vacation days
+  let [open, setOpen] = useState(false);
 
   
   const loggedIn =
@@ -28,11 +32,10 @@ const OverviewComponent = (props: any) => {
   const loggedInAdmin = user && user.hasOwnProperty("isAdmin") && user.isAdmin === 1;
   const loggedInSuperUser = user && user.hasOwnProperty("isAdmin") && user.isAdmin === 2;
 
-  const [open, setOpen] = React.useState(false);
   const handleToggle = () => {
     setOpen(prevOpen => !prevOpen);
   };
-  const anchorRef = React.useRef<HTMLButtonElement>(null);
+  const anchorRef = useRef<HTMLButtonElement>(null);
 
   /* Runs first to get all information from server*/
   useEffect(() => {
@@ -45,7 +48,6 @@ const OverviewComponent = (props: any) => {
   async function getFromServer(id: any) {
     await API.vacationsDenied(id)
       .then((response: any) => {
-        console.log(response)
         if(response.status === 200) {
           // addResponseDataToLi only accepts array input, thus convert single object to an array with single element
           let tempArr = []
@@ -71,7 +73,6 @@ const OverviewComponent = (props: any) => {
 
     await API.vacationsApproved(id)
       .then((response: any) => {
-        console.log(response)
         if(response.status === 200) {
           addResponseDataToLi(response.data, setApprovedVacationsdays);
           previousDate(response.data, setPrevious);
@@ -112,12 +113,13 @@ const OverviewComponent = (props: any) => {
     var now = new Date();
     var arr = []; // Is used for temporary storing for setting state after loop is done
     let i = 0; // Unique key for html elements
+    let temp = false
     for (let obj of response) {
       for (let date of obj.dates) {
         i++;
-        console.log(date);
         let dateOnly = date.substring(0, date.indexOf("T"));
         if(now > new Date(date)){
+          temp = true
           const liElement = (
               <Link key={i} to={{ pathname: "/requests/" + obj.requestId }}>
                 <li>{dateOnly}</li>
@@ -127,11 +129,13 @@ const OverviewComponent = (props: any) => {
           }
       }
       // separate requests in new lines instead of having all of them in one
-      arr.push(<br key={++i}></br>)
+      if(temp) {
+        arr.push(<br key={++i}></br>)
+        temp=false
+      }
     }
     where(arr);
   }
-
 
   // Adds the information to html element. 
   function addResponseDataToLi(response: any, where: any) {
@@ -141,7 +145,6 @@ const OverviewComponent = (props: any) => {
     for (let obj of response) {
       for (let date of obj.dates) {
         i++;
-        console.log(date);
         let dateOnly = date.substring(0, date.indexOf("T"));
         if (now < new Date(date)) {
         const liElement = (
@@ -174,19 +177,22 @@ const OverviewComponent = (props: any) => {
           <div>
             <h1>Upcoming vacation days</h1>
             <ul>{approvedVacationdays}</ul>
-            {/* <div> */}
-            {/* Toggle button */}
-            <Button className={vacationStyles.collapseButton}
-              onClick={handleToggle}
-            >
-              <ul><b>Show previous days</b></ul>
-            </Button>
-            <Collapse in={open} timeout="auto" unmountOnExit>
-            <ul>{previousVacationdays}</ul>
-            </Collapse>
-          {/* </div> */}
+       
+            {previousVacationdays.length > 0 && (
+              <>
+                <Button className={vacationStyles.collapseButton}
+                  onClick={handleToggle}
+                >
+                  <ul><b>Show previous vacation days</b></ul>
+                  <FontAwesomeIcon pull="right" size="lg" icon="angle-double-down"/>
+                </Button>
+                <Collapse in={open} timeout="auto" unmountOnExit>
+                <ul>{previousVacationdays}</ul>
+                </Collapse>
+              </>
+            )}
           </div>
-
+            
           <div>
             <h1>Denied vacation days</h1>
             {totalDeniedVacationRequests > 0 && (
