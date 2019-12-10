69c3bd2ad55323c1eccf55002ad8fa555f14a5bb
Jesper <jesper_truedsson@hotmail.com>
2019-12-10 16:07:02 +0100

    working on making profile view and sidenavbar responsive

--- src/views/profile/ProfileView.tsx
+++ src/views/profile/ProfileView.tsx
@@ -1,6 +1,9 @@
 import React, { useState } from 'react';
 import profileStyle from '../../css/profile/ProfileView.module.css';
 
+/* Material ui */
+import Grid from '@material-ui/core/Grid';
+
 // Specific components regarding profile
 import OverviewComponent from '../../components/profile/OverviewComponent';
 import SideBarComponent from '../../components/profile/SideBarComponent';
@@ -14,13 +17,20 @@ const ProfileView = (props:any) => {
 
     return(
         <main className={profileStyle.ProfileWrapper}>
-            <aside>
-                <SideBarComponent email={email} setEmail={setEmail}/>
-                <SettingComponent email={email}/>
-            </aside>
-            <div>
-                <OverviewComponent/>
-            </div>
+            <Grid container>
+                <Grid item xs={12} sm={4} md={4} lg={4} xl={4}>
+                    <div className={profileStyle.sidebarContainer}>
+                        <SideBarComponent email={email} setEmail={setEmail}/>
+                        <SettingComponent email={email}/>
+                    </div>
+                </Grid>
+               
+                <Grid item xs={12} sm={8} md={8} lg={8} xl={8}>
+                    <div className={profileStyle.overviewContainer}>
+                        <OverviewComponent/>
+                    </div>
+                </Grid>
+            </Grid>
         </main>  
     )
 }
