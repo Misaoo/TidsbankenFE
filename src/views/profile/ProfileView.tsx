fcf19b10e05500e3086414f7c76ec5329d5ca11c
Jesper <jesper_truedsson@hotmail.com>
2019-12-11 12:10:17 +0100

    Now the profile view is responsive and made other good changes to navbar

--- src/views/profile/ProfileView.tsx
+++ src/views/profile/ProfileView.tsx
@@ -7,7 +7,7 @@ import Grid from '@material-ui/core/Grid';
 // Specific components regarding profile
 import OverviewComponent from '../../components/profile/OverviewComponent';
 import SideBarComponent from '../../components/profile/SideBarComponent';
-import SettingComponent from '../../components/profile/SettingComponent';
+
 
 type MyProps = { }
 type MyState = { }
@@ -18,14 +18,13 @@ const ProfileView = (props:any) => {
     return(
         <main className={profileStyle.ProfileWrapper}>
             <Grid container>
-                <Grid item xs={12} sm={4} md={4} lg={4} xl={4}>
+                <Grid item xs={12} sm={4} md={4} lg={2} xl={2} className={profileStyle.gridColor1}>
                     <div className={profileStyle.sidebarContainer}>
                         <SideBarComponent email={email} setEmail={setEmail}/>
-                        <SettingComponent email={email}/>
                     </div>
                 </Grid>
                
-                <Grid item xs={12} sm={8} md={8} lg={8} xl={8}>
+                <Grid item xs={12} sm={8} md={8} lg={10} xl={10} className={profileStyle.gridColor2}>
                     <div className={profileStyle.overviewContainer}>
                         <OverviewComponent/>
                     </div>
